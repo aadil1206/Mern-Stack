464de8eec997b389dc31bee8b733062dc2223c06
Md Adil Ashrafi <105966902+aadil1206@users.noreply.github.com>
2025-07-14 22:25:27 +0530

    create folder structure mvc for backend and use basic route with express

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+
+
+export const signup = async (req, res) => {
+res.send('signup')
+};
+
+export const login = async (req, res) => {
+ res.send('login')
+};
+
+export const logout = (req, res) => {
+ res.send('logout')
+};
+
+
