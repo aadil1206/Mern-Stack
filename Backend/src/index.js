e655d51ffd01281f076814e3d11598fe644ae34a
Md Adil Ashrafi <105966902+aadil1206@users.noreply.github.com>
2025-07-15 21:13:07 +0530

    connect db with backend and make model for users

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,14 +1,20 @@
 
 import express from 'express';
 import authRoutes from "./routes/auth.route.js";
+import dotenv from 'dotenv';
+import { connectDB } from "./lib/db.js";
 
 
 
+
+dotenv.config();
 const app = express();
 
 app.use("/api/auth", authRoutes);
 
+const PORT = process.env.PORT
 
-app.listen(5001,()=>{
-    console.log("server is running");
+app.listen(PORT,()=>{
+    console.log("server is running :" + PORT);
+    connectDB();
 })
